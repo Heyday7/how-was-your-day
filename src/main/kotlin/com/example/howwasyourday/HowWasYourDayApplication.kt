d9bcd0c87d401a7068acc0fc9c8a1a580329de95
Taehyup Ko <kth3447@gmail.com>
2022-11-14 17:34:24 +0900

    first commit (create project)

---
+++ src/main/kotlin/com/example/howwasyourday/HowWasYourDayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.howwasyourday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HowWasYourDayApplication
+
+fun main(args: Array<String>) {
+	runApplication<HowWasYourDayApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/howwasyourday/HowWasYourDayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.howwasyourday
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HowWasYourDayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
