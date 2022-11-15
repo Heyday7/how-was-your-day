248198697af27243e130cc83db0c0fb92aef0037
Heyday7 <kth3447@gmail.com>
2022-11-15 17:26:32 +0900

    WebSecurityConfigurerAdapter deprecated 대응
    후일을 위해 이전 코드도 살려는 둔다.

--- src/main/kotlin/com/example/howwasyourday/config/auth/SecurityConfig.kt
+++ src/main/kotlin/com/example/howwasyourday/config/auth/SecurityConfig.kt
@@ -2,19 +2,44 @@ package com.example.howwasyourday.config.auth
 
 import com.example.howwasyourday.Role
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.SecurityFilterChain
+
+//@EnableWebSecurity
+//class SecurityConfig(
+//    @Autowired private val customOAuth2UserService: CustomOAuth2UserService
+//): WebSecurityConfigurerAdapter() {
+//    override fun configure(http: HttpSecurity?) {
+//        if (http == null) throw Exception()
+//
+//        http
+//            .csrf().disable()
+//            .headers().frameOptions().disable()
+//            .and()
+//                .authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/api/**").hasRole(Role.USER.name)
+//                .anyRequest().authenticated()
+//            .and()
+//                .logout().logoutSuccessUrl("/")
+//            .and()
+//                .oauth2Login()
+//                .userInfoEndpoint()
+//                .userService(customOAuth2UserService)
+//    }
+//}
 
 @EnableWebSecurity
 class SecurityConfig(
     @Autowired private val customOAuth2UserService: CustomOAuth2UserService
-): WebSecurityConfigurerAdapter() {
-
-    override fun configure(http: HttpSecurity?) {
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity?): SecurityFilterChain {
         if (http == null) throw Exception()
 
-        http
+        return http
             .csrf().disable()
             .headers().frameOptions().disable()
             .and()
@@ -28,5 +53,7 @@ class SecurityConfig(
                 .oauth2Login()
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService)
+            .and().and()
+            .build()
     }
 }
