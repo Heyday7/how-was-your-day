171528018796f7eaccc9660f15188ee8ef72b2cb
Heyday7 <kth3447@gmail.com>
2022-11-17 17:53:52 +0900

    Diary 생성 시 Action도 함께 넣을 수 있도록 수정

--- src/main/resources/static/js/app/diary-post.js
+++ src/main/resources/static/js/app/diary-post.js
@@ -1,16 +1,48 @@
 var main = {
     init : function () {
         var _this = this;
+
         $('#btn-save').on('click', function() {
             _this.save();
-        })
+        });
+
+        $('#btn-action-add').on('click', function() {
+            var html = `<p><input name="action[][comment]" type="text">
+                        <select name="action[][type]">
+                            <option value="Work">Work</option>
+                            <option value="Study">Study</option>
+                            <option value="Exercise">Exercise</option>
+                            <option value="Hobby">Hobby</option>
+                            <option value="Any">Any</option>
+                        </select>
+                        <button type="button" id="btn-action-delete">삭제</button></p>`
+            $('#action-list').append(html);
+        });
+        $(document).ready(function () {
+            $(document).on('click', "button[id='btn-action-delete']" ,function() {
+                $(this).parent().remove();
+            });
+        });
     },
 
     save : function () {
+
+        var actionCnt = $('input[name="comment[]"]').length;
+        var actions = new Array(actionCnt);
+        for (var i =0; i < actionCnt; i++) {
+            var action = {
+                comment: $('input[name="comment[]"]')[i].value,
+                type: $('select[name="type[]"]')[i].value
+            }
+            actions[i] = action
+        };
+
         var data = {
             title: $('#title').val(),
-            body: $('#body').val()
+            body: $('#body').val(),
+            actions: actions
         };
+
         $.ajax({
             type: 'POST',
             url: '/api/diaries',
@@ -25,5 +57,6 @@ var main = {
         })
     },
 };
+
 
 main.init();
